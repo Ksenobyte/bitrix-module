0d5e105eb9d3747904db97ab1bdfab96632015a2
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-09-12 19:02:40 +0400

    after merge fix v0.4.0

--- intaro.intarocrm/updater.php
+++ intaro.intarocrm/updater.php
@@ -9,4 +9,16 @@ if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/catal
 }
 $updater->CopyFiles("install/export/intarocrm_setup.php", "php_interface/include/catalog_export/intarocrm_setup.php");
 
-COption::SetOptionString('intaro.intarocrm', 'sites_ids', serialize(array()));
+COption::SetOptionString('intaro.intarocrm', 'sites_ids', 0);
+
+$dateAgent = new DateTime();
+$intAgent = new DateInterval('PT60S'); // PT60S - 60 sec;
+$dateAgent->add($intAgent);
+
+CAgent::AddAgent(
+        "ICrmOrderActions::orderHistoryAgent();", 'intaro.intarocrm', "N", 600, // interval - 10 mins
+        $dateAgent->format('d.m.Y H:i:s'), // date of first check
+        "Y", // агент активен
+        $dateAgent->format('d.m.Y H:i:s'), // date of first start
+        30
+);
