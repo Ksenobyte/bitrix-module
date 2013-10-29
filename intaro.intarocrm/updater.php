261dcdc75cb2237eab289c1fc4c3418be1497b1b
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-10-29 20:15:00 +0400

    updater upd

--- intaro.intarocrm/updater.php
+++ intaro.intarocrm/updater.php
@@ -1,5 +1,11 @@
 <?php
 $mid = 'intaro.intarocrm';
 $CRM_ORDER_HISTORY_DATE = 'order_history_date';
+$CRM_PAYMENT_STATUSES = 'pay_statuses_arr';
 
 COption::SetOptionString($mid, $CRM_ORDER_HISTORY_DATE, date('Y-m-d H:i:s'));
+
+$optionsPayStatuses = unserialize(COption::GetOptionString($mid, $CRM_PAYMENT_STATUSES, 0)); // --statuses
+$optionsPayStatuses['YY'] = $optionsPayStatuses['Y'];
+unset($optionsPayStatuses['Y']);
+COption::SetOptionString($mid, $CRM_PAYMENT_STATUSES, serialize($optionsPayStatuses));
