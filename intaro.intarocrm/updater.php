b17c689c3ddce2f1890758f6850aae3973b78746
Grigory Pomadchin <gr.pomadchin@gmail.com>
2014-11-25 16:00:59 +0300

    compatibility fix

--- intaro.intarocrm/updater.php
+++ intaro.intarocrm/updater.php
@@ -9,19 +9,6 @@ if(mkdir($_SERVER['DOCUMENT_ROOT'] . '/retailcrm/')) {
 		$_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/' . $mid . '/install/retailcrm/', $_SERVER['DOCUMENT_ROOT'] . '/retailcrm/', true, true
         );
 }
-// clear old agents
-CAgent::RemoveAgent("ICrmOrderActions::ordersAgent();", $mid);
-// set orderAgent
-$dateAgent = new DateTime();
-$intAgent = new DateInterval('PT60S'); // PT60S - 60 sec;
-$dateAgent->add($intAgent);
-CAgent::AddAgent(
-    "ICrmOrderActions::forkedOrderAgent();", $mid, "N", 600, // interval - 10 mins
-    $dateAgent->format('d.m.Y H:i:s'), // date of first check
-    "Y", // agent is active
-    $dateAgent->format('d.m.Y H:i:s'), // date of first start
-    30
-);
 
 $api_host = COption::GetOptionString($mid, $CRM_API_HOST_OPTION, 0);
 
